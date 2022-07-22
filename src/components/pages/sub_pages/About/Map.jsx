050d191d4ae859e9faf5951703e8d7bd722f48ba
Sarah <sarahnnanyelugo@gmail.com>
2022-07-22 12:55:18 +0100

    map replacement

--- src/components/pages/sub_pages/About/Map.jsx
+++ src/components/pages/sub_pages/About/Map.jsx
@@ -5,9 +5,8 @@ function Map() {
   return (
     <div className="col-md-6">
 
-      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d53344.696448717244!2d7.480591924161454!3d6.410962392397635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1severgreen%20school%20enugu!5e0!3m2!1sen!2sng!4v1654514748253!5m2!1sen!2sng" width="100%" height="750"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-{/* <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d3964.6047299087977!2d7.506874515266624!3d6.444769545337405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d6.445053799999999!2d7.505117899999999!4m5!1s0x1044a3e4bc0fe0d9%3A0xbc5c62f566b0ab76!2s3%20Presidential%20Cl%2C%20Independence%20Layout%20400102%2C%20Enugu!3m2!1d6.4440623!2d7.5126174!5e0!3m2!1sen!2sng!4v1658367987192!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   */}
-
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.610257657395!2d7.510428715266626!3d6.444062295337873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a3e4bc0fe0d9%3A0xbc5c62f566b0ab76!2s3%20Presidential%20Cl%2C%20Independence%20Layout%20400102%2C%20Enugu!5e0!3m2!1sen!2sng!4v1658490648810!5m2!1sen!2sng" width="100%" height="750"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.610257657395!2d7.510428715266626!3d6.444062295337873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a3e4bc0fe0d9%3A0xbc5c62f566b0ab76!2s3%20Presidential%20Cl%2C%20Independence%20Layout%20400102%2C%20Enugu!5e0!3m2!1sen!2sng!4v1658490648810!5m2!1sen!2sng" width="100%" height="750" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
   </div>
   )
 }
